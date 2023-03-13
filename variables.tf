efac0ba584456c6b210b219b7f6bcf0603a3d461
isfonzar <icaro.isf@gmail.com>
2023-03-13 20:54:32 -0300

    Use variables for project name and description

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "ssh_key" {
   type      = string
   sensitive = true
 }
+
+# Project related variables
+variable "cluster_name" {
+  type    = string
+  default = "Cluster"
+}
+variable "cluster_description" {
+  type    = string
+  default = "Cluster of nodes"
+}

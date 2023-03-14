021861a79e4445aa0c70e37bb3bc7b5a36745f0d
isfonzar <icaro.isf@gmail.com>
2023-03-13 23:24:23 -0300

    Add worker public ips to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "master_public_ip" {
   value       = digitalocean_droplet.master.ipv4_address
   description = "The public IP address of the master node"
 }
+
+output "worker_public_ips" {
+  value       = digitalocean_droplet.worker[*].ipv4_address
+  description = "Public IP addresses of the worker nodes"
+}
